e3e6fbdace62c6115356cbe5caf4000bb2475a3e
Darshan-Kantesariya <darshank.softrefine@gmail.com>
2022-11-07 17:35:05 +0530

    non responsive complete

--- script.js
+++ script.js
@@ -101,3 +101,69 @@ function btn3(){
     document.querySelector(".card-rim-box-button-3").style.backgroundColor="var(--color-card-1)";
 
 }
+
+function myFunction() {
+    var x = document.getElementById("myCheck").checked;
+    if (x == false) {
+        document.querySelector(".side-task-div-4").classList.remove("bg-color");
+        document.getElementById("changeco4").style.stroke="#f0884e";
+        document.getElementById("changeco2").style.stroke="#f0884e";
+        document.getElementById("changeco3").style.stroke="#f0884e";
+        document.querySelector(".side-task-div-2-text").classList.remove("text-white");
+        document.querySelector(".side-task-div-2-title-2").classList.remove("text-white");
+
+    }
+    else {
+        document.getElementById("changeco4").style.stroke="#fff";
+        document.getElementById("changeco2").style.stroke="#fff";
+        document.getElementById("changeco3").style.stroke="#fff";
+        document.querySelector(".side-task-div-2-text").classList.add("text-white");
+        document.querySelector(".side-task-div-2-title-2").classList.add("text-white");
+        document.querySelector(".side-task-div-4").classList.add("bg-color");
+    }
+}
+
+
+function myFunction1() {
+    var x = document.getElementById("myCheck1").checked;
+    if (x == false) {
+        document.querySelector(".side-task-div-2-text1").classList.remove("text-white");
+        document.querySelector(".side-task-div-2-title-21").classList.remove("text-white");
+        document.querySelector(".side-task-div-3").classList.remove("bg-color");
+        document.getElementById("changeco5").style.border="1px solid var(--color-side-card)";
+        document.getElementById("changeco6").style.border="1px solid var(--color-side-card)";
+        document.getElementById("changeco7").style.border="1px solid var(--color-side-card)";
+
+    }
+    else {
+        document.querySelector(".side-task-div-2-text1").classList.add("text-white");
+        document.querySelector(".side-task-div-2-title-21").classList.add("text-white");
+        document.querySelector(".side-task-div-3").classList.add("bg-color");
+        document.getElementById("changeco5").style.border="1px solid white";
+        document.getElementById("changeco6").style.border="1px solid white";
+        document.getElementById("changeco7").style.border="1px solid white";
+        
+        
+
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById("myCheck2").checked;
+    if (x == false) {
+        document.querySelector(".side-task-div-2").classList.remove("bg-color");
+        document.getElementById("changeco").style.stroke="#f0884e";
+        document.getElementById("changeco1").style.stroke="#f0884e";
+        document.querySelector(".side-task-div-2-text2").classList.remove("text-white");
+        document.querySelector(".side-task-div-2-title-22").classList.remove("text-white");
+
+    }
+    else {
+        document.querySelector(".side-task-div-2").classList.add("bg-color");
+        document.getElementById("changeco").style.stroke="white";
+        document.getElementById("changeco1").style.stroke="white";
+        document.querySelector(".side-task-div-2-text2").classList.add("text-white");
+        document.querySelector(".side-task-div-2-title-22").classList.add("text-white");
+
+    }
+}
